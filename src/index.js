ce7f640fff1a0c6d5b1a00da2bd5bd0747926690
x0n4d0 <romagnolimauricio@gmail.com>
2020-11-10 17:30:20 -0300

    feat: clean the project and leave it blank

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
